a2caee8df4df7a988b006aa5375a959be859d8b9
Rafael Campos <rafacampos2000@hotmail.com>
2021-03-25 14:46:14 -0300

    adding use list and detail routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,16 +6,16 @@ const router = express.Router()
 
 router.get(
     '/', (req, res) => {
-        res.send('Hello World')
+        res.send('Hello World');
     })
 
 router.post(
     '/userSignup',
     async (req, res) => {
-        const name= req.body.name
-        const age= req.body.age
-        const gender= req.body.gender
-        const dob= (req.body.dob)
+        const name = req.body.name
+        const age = req.body.age
+        const gender = req.body.gender
+        const dob = req.body.dob
         await UserModel.create({ name, age, gender, dob });
         try {
             res.json("User created with success")
@@ -26,5 +26,27 @@ router.post(
     }
 )
 
+router.get(
+    '/list',
+    async (req, res) => {
+        const all = await UserModel.find();
+        try {
+            res.json(all);
+        } catch (error) {
+            res.json(error);
+        }
+
+    }
+)
+
+router.get(
+    '/detailPerson',
+    async (req, res) => {
+        const id = req.body.person_Id;
+        await UserModel.findById(id).exec(function (err, user) {
+            res.json(user);
+        })
+    }
+)
 
 module.exports = router;
